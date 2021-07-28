fa8434b037936d9e35dfd1e64e76612e262eb5ce
Amit Porwal <aporwal6@csc.com>
2021-07-28 10:21:24 +0530

    new1

--- src/products.ts
+++ src/products.ts
@@ -1,15 +1,15 @@
 export const products = [
     {
-      name: 'Sugar',
-      price: 79,
-      description: 'Madhu sortex clean and organic sugar',
-      image:'/assets/images/sugar.jpg' 
+      name: 'Tea',
+      price: 249,
+      description: 'Assam Imported Tea, organic',
+      image: '/assets/images/tea.jpg'
     },
     {
-      name: 'Oil',
-      price: 100,
-      description: 'Refined free flow oil,with added mineals',
-      image:'/assets/images/oil.jpg'
+      name: 'Tea',
+      price: 249,
+      description: 'Assam Imported Tea, organic',
+      image: '/assets/images/tea.jpg'
     },
     {
       name: 'Tea',
