47411fca30c955e26e60d664ec0d9d2357c4ff3e
Amit Porwal <aporwal6@csc.com>
2021-08-04 19:22:05 +0530

    images load trial

--- docs/main.js
+++ docs/main.js
@@ -284,44 +284,44 @@ const products = [
         name: 'Tea',
         price: 249,
         description: 'Assam Imported Tea, organic',
-        image: '/assets/images/tea.jpg'
+        image: 'ecom/assets/images/tea.jpg'
     },
     {
         name: 'Tea',
         price: 249,
         description: 'Assam Imported Tea, organic',
-        image: '/assets/images/tea.jpg'
+        image: 'ecom/assets/images/tea.jpg'
     },
     {
         name: 'Tea',
         price: 249,
         description: 'Assam Imported Tea, organic',
-        image: '/assets/images/tea.jpg'
+        image: 'ecom/assets/images/tea.jpg'
     },
     {
         name: 'Tea',
         price: 249,
         description: 'Assam Imported Tea, organic',
-        image: '/assets/images/tea.jpg'
+        image: 'ecom/assets/images/tea.jpg'
     },
     {
         name: 'Tea',
         price: 249,
         description: 'Assam Imported Tea, organic',
-        image: '/assets/images/tea.jpg'
+        image: 'ecom/assets/images/tea.jpg'
     },
     {
         name: 'Tea',
         price: 249,
         description: 'Assam Imported Tea, organic',
-        image: '/assets/images/tea.jpg'
+        image: 'ecom/assets/images/tea.jpg'
     },
 ];
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
 can be found in the LICENSE file at http://angular.io/license
-*/ 
+*/
 
 
 /***/ }),
